af68ac8c75ef1ba198a1287973c5a9cd03c0cb80
Regan S Poston <mr.rsposton@gmail.com>
2014-08-12 15:00:06 -0700

    created a wider graph with more points

--- app/helpers/graphs_helper.rb
+++ app/helpers/graphs_helper.rb
@@ -1,17 +1,17 @@
 module GraphsHelper
   def get_graph_labels
     array_of_labels=[]
-    start = 48.hours.ago.in_time_zone("Pacific Time (US & Canada)").beginning_of_hour
+    start = 96.hours.ago.in_time_zone("Pacific Time (US & Canada)").beginning_of_hour
     finish = Time.now.in_time_zone("Pacific Time (US & Canada)").beginning_of_hour
     while(start <= finish) do
-      array_of_labels << start.to_s
+      (start.time.to_s.include? "12:00") ? (array_of_labels << start.to_s) : (array_of_labels << '')
       start += 1.hour
     end
     array_of_labels
   end
   def get_readings
     array_of_readings=[]
-    start = 48.hours.ago.beginning_of_hour
+    start = 96.hours.ago.beginning_of_hour
     finish = Time.now.beginning_of_hour
     while(start <= finish) do
       last_hour_reading = Reading.where("recorded_at between ? and ?", start, start + 1.hour)
