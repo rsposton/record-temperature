f4aff7691a92b26565ffd2f2232bf0c120b1326b
Regan S Poston <mr.rsposton@gmail.com>
2014-08-13 21:35:40 -0700

    longer time line

--- app/helpers/graphs_helper.rb
+++ app/helpers/graphs_helper.rb
@@ -1,7 +1,7 @@
 module GraphsHelper
   def get_graph_labels
     array_of_labels=[]
-    start = 96.hours.ago.in_time_zone("Pacific Time (US & Canada)").beginning_of_hour
+    start = 200.hours.ago.in_time_zone("Pacific Time (US & Canada)").beginning_of_hour
     finish = Time.now.in_time_zone("Pacific Time (US & Canada)").beginning_of_hour
     while(start <= finish) do
       (start.time.to_s.include? "12:00") ? (array_of_labels << start.to_s) : (array_of_labels << '')
@@ -11,7 +11,7 @@ module GraphsHelper
   end
   def get_readings
     array_of_readings=[]
-    start = 96.hours.ago.beginning_of_hour
+    start = 200.hours.ago.beginning_of_hour
     finish = Time.now.beginning_of_hour
     while(start <= finish) do
       last_hour_reading = Reading.where("recorded_at between ? and ?", start, start + 1.hour)
